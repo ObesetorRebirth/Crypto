2a136dcd2bdaf11c04c2cbda66622ad172767353
ObesetorRebirth <v76850279@gmail.com>
2025-04-05 18:27:13 +0300

    Update UserCryptoService.java

--- src/main/java/com/example/Crypto/Services/UserCryptoService.java
+++ src/main/java/com/example/Crypto/Services/UserCryptoService.java
@@ -1,4 +1,14 @@
 package com.example.Crypto.Services;
 
+import com.example.Crypto.Repositories.UserCryptoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+@Transactional
 public class UserCryptoService {
+
+    private final UserCryptoRepository userCryptoRepository;
 }
